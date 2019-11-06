1c6f7d001b34c83c54b29f3cd3380349443c9d62
daniel <arquitectura.conacyt@conacyt.mx>
2019-11-05 19:14:06 -0600

    updateing snippets

--- features/websocket_features.js
+++ features/websocket_features.js
@@ -5,11 +5,8 @@
 module.exports = function(controller) {
 
     if (controller.adapter.name === 'Web Adapter') {
-
         console.log('Loading sample web features...');
-
         controller.hears(new RegExp('quick'), 'message', async (bot, message) => {
-
             await bot.reply(message,{
                 text: 'Here are some quick replies',
                 quick_replies: [
@@ -24,8 +21,5 @@ module.exports = function(controller) {
                 ]
             });
         });
-
-
     }
-
 }
