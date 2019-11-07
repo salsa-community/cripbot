b1891df0aee3eb3d8e0bacd4d6ae34e61f312c0b
daniel <arquitectura.conacyt@conacyt.mx>
2019-11-07 15:27:58 -0600

    bugfix update dialog

--- features/dialogs/rfc-dialog.js
+++ features/dialogs/rfc-dialog.js
@@ -18,12 +18,27 @@ module.exports = function (controller) {
     convo.addQuestion(RFC_ASK, async (res, convo, bot) => {
         if (res === 'SAMI760605RH6') {
             bot.say({ text: 'Bienvenida Itzia María del Carmen Sánchez Méndez al soporte de RedCofidi' });
-            bot.beginDialog(TICKET_DIALOG_ID);
+            await convo.gotoThread('ticket-thread');
         } else {
             bot.say({ text: 'el RFC que me proporcionó no se encuentra en nuestra lista de clientes' });
             await convo.gotoThread('rfc-thread');
         }
     }, 'rfc', 'rfc-thread');
+
+    convo.addAction('ticket-thread');
+    convo.addQuestion('Por favor, ingrese el número de ticket', async (res, convo, bot) => {
+        if (res === 'cfdi-123') {
+            bot.say({ text: 'El problema es que en su factura no se está colocando el Uso de CFDI, para corregirlo debe de realizar el siguiente proceso:' });
+            bot.say({ text: 'Ingresar a su factura y presione la opción de modificar' });
+            bot.say({ text: 'Buscar la opción de “Uso de CFDI”' });
+            bot.say({ text: 'Posterior dar clic en la lupa a lado de este campo, y seleccionar el tipo de uso de CFDI de acuerdo a la lista desplegable y dar “aceptar”' });
+            bot.say({ text: 'Finalmente dar clic en “Actualizar” y volver a autorizar.' });
+        } else {
+            bot.say({ text: 'el ticket ingresado no se encuentra en nuestra base de conocimiento' });
+            await convo.gotoThread('ticket-thread');
+        }
+    }, 'ticket', 'ticket-thread');
+
 
     controller.addDialog(convo);
     controller.hears('red-cofidi', 'message', async (bot, message) => {
