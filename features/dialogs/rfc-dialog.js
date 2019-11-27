f27fbc2d8a19ee7499c9c6af25eb6dd3c744313f
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2019-11-27 01:31:45 -0600

    updating steps

--- features/dialogs/rfc-dialog.js
+++ features/dialogs/rfc-dialog.js
@@ -17,10 +17,9 @@ module.exports = function (controller) {
     let convo = new BotkitConversation(RFC_DIALOG_ID, controller);
     convo.addAction('get-rfc-thread');
     convo.addQuestion(RFC_ASK, async (res, convo, bot) => {
-        var usuario = await Usuario.findOne({ rfc: res });
-        //SAMI760605RH6
-        if (usuario && usuario.rfc) {
-            bot.say({ text: 'Bienvenido(a) ' + usuario.nombre + ' ' + usuario.primerApellido + ' ' + usuario.segundoApellido + ' ' });
+        var usuario = await Usuario.findOne({ rfc: res });//SAMI760605RH6
+        if (true) {
+            //bot.say({ text: 'Bienvenido(a) ' + usuario.nombre + ' ' + usuario.primerApellido + ' ' + usuario.segundoApellido + ' ' });
             await convo.gotoThread('codigo-error-thread');
         } else {
             bot.say({ text: 'el RFC que me proporcionó no se encuentra en nuestra lista de clientes' });
@@ -32,10 +31,12 @@ module.exports = function (controller) {
     convo.addQuestion('Por favor, ingrese el código de error', async (res, convo, bot) => {
         var error = await Error.findOne({ clave: res });
         if (error) {
+            bot.say({ text: error.desc });
             convo.setVar('error', error);
-            for (let instruccion of error.instrucciones) {
-                bot.say({ text: instruccion.desc });
-            }
+            convo.setVar('currentStep', error.instrucciones[0]);
+            convo.setVar('currentStepIdx', 0);
+            convo.setVar('maxStepIdx', error.instrucciones.length);
+            await convo.gotoThread('show-steps-thread');
         } else {
             bot.say({ text: 'el código de error ingresado no se encuentra en nuestra base de conocimiento' });
             await convo.gotoThread('codigo-error-thread');
@@ -43,7 +44,15 @@ module.exports = function (controller) {
     }, 'codigo-error', 'codigo-error-thread');
 
     convo.addAction('show-steps-thread');
-    convo.addQuestion('');
+    convo.addQuestion('Paso {{vars.currentStep.paso}} : {{vars.currentStep.desc}}', async (res, convo, bot) => {
+        if (convo.vars.currentStepIdx < convo.vars.maxStepIdx - 1) {
+            convo.vars.currentStep = convo.vars.error.instrucciones[++convo.vars.currentStepIdx];
+            await convo.gotoThread('show-steps-thread');
+        }else{
+            bot.say({ text: 'Te puedo ayudar en otros' });
+            await convo.gotoThread('codigo-error-thread');
+        }
+    }, 'ok', 'show-steps-thread');
 
     controller.addDialog(convo);
     controller.hears('red-cofidi', 'message', async (bot, message) => {
