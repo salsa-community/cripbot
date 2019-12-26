3055770b65fabb97a756c0308f294591e5734261
daniel <arquitectura.conacyt@conacyt.mx>
2019-12-26 16:30:26 -0600

    update Usuario.model

--- models/Usuario.model.js
+++ models/Usuario.model.js
@@ -4,8 +4,8 @@ const Schema = mongoose.Schema;
 let UsuarioSchema = new Schema({
     rfc: { type: String, required: true, max: 100 },
     nombre: { type: String, required: true, max: 100 },
-    primerApellido: { type: String, required: true, max: 100 },
-    segundoApellido: { type: String, required: true, max: 100 }
+    primer_apellido: { type: String, required: true, max: 100, alias: 'primerApellido' },
+    segundo_apellido: { type: String, required: true, max: 100, alias: 'segundoApellido' }
 });
 
 
