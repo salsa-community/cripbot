387b638cfe061a68b51c8fdc6826e26fb513e615
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2020-08-04 13:29:16 -0500

    update bot refactoring

--- public/bot-redcofidi.js
+++ public/bot-redcofidi.js
@@ -6,12 +6,17 @@
 
 let appKey = "RedCofidi";
 
+let home = document.getElementById("bot-client-script").getAttribute("bothome");
+if (!home) {
+  home = "";
+}
+
 let options = { id: appKey, asistente: Botkit.getAsistente() };
 Botkit.boot(options);
 Botkit.deactivate();
 setTimeout(
   function () {
-    document.getElementById("botkit_client").src = "/index.html";
+    document.getElementById("botkit_client").src = home + "/index.html";
     setTimeout(
       function () {
         Botkit.activate();
