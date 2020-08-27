4d3fb6f8406e1819fe06656ebd59f90cfe0da633
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2020-08-27 13:36:30 -0500

    new client sivs

--- features/dialogs/util/chat-commons.js
+++ features/dialogs/util/chat-commons.js
@@ -1,7 +1,7 @@
 const { getRandomInt, resolveGreeting } = require('../../../util/commons');
 const personal = ['María', 'Isabella', 'Jimena', 'Laura'];
 const { infoQuickReplies } = require('./info-quick-replies');
-const { BOT_CLIENT_RED_COFIDI__ID } = require('./constants')
+const { BOT_CLIENT_RED_COFIDI__ID, BOT_CLIENT_PAC_WEB__ID } = require('./constants')
 const { typing } = require('../../../util/bot.typing');
 
 /**
@@ -12,8 +12,10 @@ exports.hello = async function (bot, message) {
     await typing(bot, message, 'Bienvenido a ATEB');
     if (message.user == BOT_CLIENT_RED_COFIDI__ID) {
         await typing(bot, message, 'Red COFIDI es un servicio para intercambiar Comprobantes Fiscales Digitales de una manera rápida y segura.');
-    } else {
+    } else if (message.user == BOT_CLIENT_PAC_WEB__ID) {
         await typing(bot, message, 'PAC Web es un servicio de ATEB para apoyar a sus clientes.');
+    } else {
+        await typing(bot, message, message.user + ' es un servicio de ATEB para apoyar a sus clientes.');
     }
     await typing(bot, message, 'Mi nombre es ' + message.user_profile.asistente);
     await typing(bot, message, {
@@ -30,8 +32,10 @@ exports.welcomeBack = async function (bot, message) {
     await typing(bot, message, 'Bienvenido de nuevo a ATEB');
     if (message.user == BOT_CLIENT_RED_COFIDI__ID) {
         await typing(bot, message, 'Red COFIDI es un servicio para intercambiar Comprobantes Fiscales Digitales de una manera rápida y segura.');
-    } else {
+    } else if (message.user == BOT_CLIENT_PAC_WEB__ID) {
         await typing(bot, message, 'PAC Web es un servicio de ATEB para apoyar a sus clientes.');
+    } else {
+        await typing(bot, message, message.user + ' es un servicio de ATEB para apoyar a sus clientes.');
     }
     await typing(bot, message, 'Mi nombre es ' + message.user_profile.asistente);
     await typing(bot, message, {
---
+++ public/bot-sivs.js
@@ -0,0 +1,24 @@
+/**
+ * Initi configuration
+ * for the bot
+ *
+ */
+
+let appKey = "sivs";
+
+let home = document.getElementById("bot-client-script").getAttribute("bothome");
+if (!home) {
+  home = "";
+}
+
+let options = { id: appKey, asistente: Botkit.getAsistente() };
+Botkit.boot(options);
+Botkit.deactivate();
+setTimeout(
+  function () {
+    document.getElementById("botkit_client").src = home + "/index.html";
+    setTimeout(
+      function () {
+        Botkit.activate();
+      }, 1000);
+  }, 1000);
