9ec6867aba45efdad1e5d7adff12eda66a2ff5ea
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2020-11-23 17:12:29 -0600

    update

---
+++ docker/run-pre-prod.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export BOT_ENV='production'
+export BOT_TAG='latest'
+export KBASE_TAG='latest'
+
+export BOT_ENV=production
+export DB_HOST='vtiger-db'
+export DB_NAME=becovtig
+export DB_USERNAME=becovtiguser
+export DB_PASSWORD='5g#k@&k2p'
+
+docker-compose -f docker-compose-pre-prod.yml up
