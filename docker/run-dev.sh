c942bd15ed56f74ef5beddc498230033fc322bb0
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2020-12-15 17:45:54 -0600

    update script

--- docker/run-dev.sh
+++ docker/run-dev.sh
@@ -4,4 +4,10 @@ export BOT_ENV='production'
 export BOT_TAG='latest'
 export KBASE_TAG='latest'
 
+export BOT_ENV=production
+export DB_HOST='vtiger-db'
+export DB_NAME=becovtig
+export DB_USERNAME=becovtiguser
+export DB_PASSWORD='5g#k@&k2p'
+
 docker-compose -f docker-compose-dev.yml up
