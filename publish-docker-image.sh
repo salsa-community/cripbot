c8b9c6327350178a54233b54729a18272b8e0bdb
danimaniarqsoft <daniel.cortes.pichardo@gmail.com>
2021-08-12 13:55:08 -0500

    update publish script

--- publish-docker-image.sh
+++ publish-docker-image.sh
@@ -3,7 +3,8 @@ PACKAGE_VERSION=$(cat package.json \
   | grep version \
   | head -1 \
   | awk -F: '{ print $2 }' \
-  | sed 's/[",]//g')
+  | sed 's/[",]//g' \
+  | xargs)
 
 docker build -t "conacyt/cripbot:${PACKAGE_VERSION}" .
 cat password.txt | docker login --username conacyt --password-stdin
