657d1a675e34045bd836ee9d4526654ce9a9ad01
danimaniarqsoft <daniel.cortes.pichardo@gmail.com>
2021-08-18 02:47:33 -0500

    feat event bot-status

--- docker/run-conacyt.sh
+++ docker/run-conacyt.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 export BOT_ENV='production'
-export BOT_TAG='2.6.1'
+export BOT_TAG='2.6.2'
 export KBASE_TAG='2.4.0'
 
 export BOT_ENV=production
 
 export DISABLE_STATE_STORAGE='true'
 export DISABLE_ANALYTICS='true'
-export CACHE_TTL='120'
+export CACHE_TTL='86400'
 
 docker-compose -f docker-compose-conacyt.yml up
