c5f4587c8c446765e0eae6cacdb240a4568e17c7
daniel <daniel.cortes.pichardo@gmail.com>
2024-04-09 06:14:16 -0600

    updating run-development

--- docker/run-development.sh
+++ docker/run-development.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+#########################
+## Variables del BOT
+#########################
+
 export BOT_ENV='production'
-export BOT_TAG='2.1.9'
-export KBASE_TAG='latest'
-
-export BOT_ENV=production
-
+export BOT_TAG='2.8.2'
 export DISABLE_STATE_STORAGE='true'
 export DISABLE_ANALYTICS='true'
 export CACHE_TTL='120'
 
-docker-compose -f docker-compose-development.yml up
+#########################
+## Variables dek KBASE
+#########################
+export KBASE_TAG='latest'
+
+docker compose -f docker-compose-development.yml up kbase-db
